503105be1b4ebd8b22854b741d2a6f2ce09d9b5e
TyrzYYmieLu <imustnothavefun@gmail.com>
2024-08-20 17:25:18 +0200

    adding file input.py

---
+++ input.py
@@ -0,0 +1,7 @@
+# input() = A function that prompts the user to enter data
+#           Returns the entered data as a string
+
+
+anwser = input("do you like carrots yes/no: ")
+print(f"so your anwser is {anwser}? ... that's interesting...")
+
