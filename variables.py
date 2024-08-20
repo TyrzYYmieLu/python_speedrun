e30cdb3cb75837d3dd79dd486324d478c894c863
TyrzYYmieLu <imustnothavefun@gmail.com>
2024-08-20 17:14:08 +0200

    adding 2nd program called variables.py

---
+++ variables.py
@@ -0,0 +1,15 @@
+'''
+variable it's a container for a value (string, int, float, boolean)
+'''
+
+# strings
+name = "bobo"
+email = "yourBRO@yourmoms.com"
+
+# int
+age = 25
+print(f"your age is {age}")
+if isinstance(age,int):
+    print("YES it is...")
+
+# 
