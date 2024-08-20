b1fefea686228eb677d39bb5a6b0d6f320b9e7d9
TyrzYYmieLu <imustnothavefun@gmail.com>
2024-08-20 17:19:39 +0200

    adding file typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,12 @@
+# Typecasting = the procrss of converting a variable from one data type to another 
+# str(), int(), float(), bool()
+
+age = 12
+gpa = 6.9
+
+is_drunk = True
+
+print(type(age))
+
+# int, float are mutable??
+age = float(age)
