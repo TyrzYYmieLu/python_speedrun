a3113d4a2ea12a4be33e666267f057000ba78536
TyrzYYmieLu <imustnothavefun@gmail.com>
2024-08-21 22:28:55 +0200

    adding string_indexing file

---
+++ string_indexing.py
@@ -0,0 +1,12 @@
+# indexing = accessing elements of a sequence using [] (indexing operator)
+#            [start : end : step]
+#            the starting index is inclusive 
+#            the ending index is exclusinve 
+#            example: number = "123467"
+#            number[0:4]    --> "1234" not "12345"
+#            number[0:]     --> "1234567"
+#            number[-1]     --> "7"
+#            number[::2]    --> "1357"
+#            number[-4:]    --> "12"
+#            number[::-1]   --> "7654321"
+
