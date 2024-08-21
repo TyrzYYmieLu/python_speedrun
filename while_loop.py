2b309ee9affe3cb37897cb73255f5244af5d19fc
TyrzYYmieLu <imustnothavefun@gmail.com>
2024-08-21 22:55:58 +0200

    added while_loop.py file

---
+++ while_loop.py
@@ -0,0 +1,14 @@
+# while loop = execute some code WHILE some condition remains true
+
+
+name = input("Enter your name: ")
+
+'''while name == "":
+    print("You did not enter your name")
+    name = input("Enter your name: ")
+print(f"Hello {name}")
+'''
+
+while not name == "kuba":
+    print("your name is not kuba")
+    name = input("Enter your name: ")
