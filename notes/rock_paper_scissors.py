71c245a2f6356e154550cfe3d8bc24fb32e831f4
TyrzYYmieLu <imustnothavefun@gmail.com>
2024-08-23 19:54:45 +0200

    Added rock_paper_scissors.py file

---
+++ notes/rock_paper_scissors.py
@@ -0,0 +1,31 @@
+# Rock, paper, scissors program
+import random
+
+options = ("rock", "paper", "scissors")
+
+pc = random.choice(options)
+
+my = input("Choose your option rock paper scissors: ")
+
+if my == "paper":
+    if pc == "scissors":
+        print("you loose")
+    elif pc == "rock":
+        print("you win")
+    else:
+        print("you tie")
+if my == "rock":
+    if pc == "scissors":
+        print("you win")
+    elif pc == "rock":
+        print("you tie")
+    else:
+        print("you loose")
+
+if my == "scissors":
+    if pc == "scissors":
+        print("you tie")
+    elif pc == "rock":
+        print("you loose")
+    else:
+        print("you win")
